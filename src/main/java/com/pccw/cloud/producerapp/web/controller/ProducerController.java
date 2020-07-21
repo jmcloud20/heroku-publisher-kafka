4de6257db9d968eda91856b3f051d3f69a3088ba
josephg <jmgarcia214@yahoo.com>
2020-07-21 17:02:22 +0800

    * Optimized imports.

--- src/main/java/com/pccw/cloud/producerapp/web/controller/ProducerController.java
+++ src/main/java/com/pccw/cloud/producerapp/web/controller/ProducerController.java
@@ -4,7 +4,6 @@ import com.pccw.cloud.producerapp.web.model.MessageDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import sun.plugin2.message.Message;
 
 @RequestMapping("/api/v1/producer")
 @RestController
